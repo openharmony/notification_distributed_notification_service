f3eb8f8ca7f78a38e98169506c47d22f78ce96bf
廖康康 <liaokangkang@huawei.com>
2022-07-30 11:41:14 +0800

    数据库目录修改
    Signed-off-by: 廖康康 <liaokangkang@huawei.com>

--- frameworks/ans/src/reminder_store.cpp
+++ frameworks/ans/src/reminder_store.cpp
@@ -29,8 +29,8 @@
 namespace OHOS {
 namespace Notification {
 namespace {
-const std::string REMINDER_DB_DIR = "/data/service/el1/public/notification/reminder_agent/";
-const std::string REMINDER_DB_NAME = "reminder.db";
+const std::string REMINDER_DB_DIR = "/data/service/el1/public/notification/";
+const std::string REMINDER_DB_NAME = "notification.db";
 const std::string REMINDER_DB_TABLE = "reminder";
 const uint32_t REMINDER_RDB_VERSION = 1;
 const int32_t STATE_FAIL = -1;
