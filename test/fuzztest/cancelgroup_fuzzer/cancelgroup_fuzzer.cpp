7a9db5220e7c2ff5ee795e7ccf9572c835582fa4
liuyanzhi <liuyanzhi3@huawei.com>
2022-10-11 17:12:45 +0800

    add cancel fuzz test
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: I11a95ca2acd5faf4534481c79d2acf629f0b6672

---
+++ test/fuzztest/cancelgroup_fuzzer/cancelgroup_fuzzer.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cancelgroup_fuzzer.h"
+
+#include "notification_helper.h"
+
+namespace OHOS {
+    namespace {
+        constexpr uint8_t ENABLE = 2;
+    }
+    bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+    {
+        // test GetShowBadgeEnabled function
+        bool enabled = *data % ENABLE;
+        Notification::NotificationHelper::GetShowBadgeEnabled(enabled);
+        // test CancelGroup function
+        std::string stringData(data);
+        Notification::NotificationHelper::CancelGroup(stringData);
+        // test SetDoNotDisturbDate function
+        uint32_t type = GetU32Data(data);
+        Notification::NotificationDoNotDisturbDate disturb;
+        Notification::NotificationConstant::DoNotDisturbType disturbType =
+            Notification::NotificationConstant::DoNotDisturbType(type);
+        disturb.SetDoNotDisturbType(disturbType);
+        Notification::NotificationHelper::SetDoNotDisturbDate(disturb);
+        // test GetDoNotDisturbDate function
+        Notification::NotificationHelper::GetDoNotDisturbDate(disturb);
+        // test DoesSupportDoNotDisturbMode function
+        Notification::NotificationHelper::DoesSupportDoNotDisturbMode(enabled);
+        // test IsDistributedEnabled function
+        return Notification::NotificationHelper::IsDistributedEnabled(enabled);
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    char *ch = ParseData(data, size);
+    if (ch != nullptr) {
+        OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+        free(ch);
+        ch = nullptr;
+    }
+    return 0;
+}
---
+++ test/fuzztest/getallactivenotifications_fuzzer/getallactivenotifications_fuzzer.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "getallactivenotifications_fuzzer.h"
+
+#include "notification_helper.h"
+
+namespace OHOS {
+    namespace {
+        constexpr uint8_t ENABLE = 2;
+    }
+    bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+    {
+        // test IsAllowedNotify function
+        std::string stringData(data);
+        int32_t usingData = static_cast<int32_t>(GetU32Data(data));
+        Notification::NotificationBundleOption bundleOption;
+        bundleOption.SetBundleName(stringData);
+        bundleOption.SetUid(usingData);
+        bool allowed = *data % ENABLE;
+        Notification::NotificationHelper::IsAllowedNotify(bundleOption, allowed);
+        // test GetNotificationSlotsForBundle function and one parameter
+        sptr<Notification::Notification> notification = nullptr;
+        std::vector<sptr<Notification::Notification>> notifications;
+        notifications.emplace_back(notification);
+        Notification::NotificationHelper::GetAllActiveNotifications(notifications);
+        // test GetNotificationSlotsForBundle function and two parameter
+        std::vector<std::string> keys;
+        keys.emplace_back(stringData);
+        return Notification::NotificationHelper::GetAllActiveNotifications(keys, notifications) == ERR_OK;
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    char *ch = ParseData(data, size);
+    if (ch != nullptr) {
+        OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+        free(ch);
+        ch = nullptr;
+    }
+    return 0;
+}
