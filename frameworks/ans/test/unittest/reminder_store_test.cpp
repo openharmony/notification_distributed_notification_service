493cefa91fee3e6f22e6fbe29a0f31610d09c5ef
wwx1143030 <wujiqin5@huawei.com>
2022-10-12 17:10:56 +0800

    IssueNo:https://gitee.com/openharmony/notification_common_event_service/issues/I5VB6V Description:分支覆盖率提升reminder_store0202 Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source: No
    Signed-off-by: wwx1143030 <wujiqin5@huawei.com>
    Change-Id: I771d6a72c76f9db5915c4eb3f7fc5445c0406205

--- frameworks/ans/test/unittest/reminder_store_test.cpp
+++ frameworks/ans/test/unittest/reminder_store_test.cpp
@@ -44,7 +44,6 @@ public:
         ReminderHelper::CancelAllReminders();
     }
     static sptr<NotificationBundleOption> bundleOption_;
-
 };
 
 sptr<NotificationBundleOption> ReminderStoreTest::bundleOption_ =
