5cc599ae0efdfd73547c84e3eb5d545a5510e20d
wujiqin <wujiqin5@huawei.com>
2022-10-20 16:21:23 +0800

    IssueNo:https://gitee.com/openharmony/notification_distributed_notification_service/issues/I5WRQ2 Description:ans覆盖率提升05 Sig:SIG_ApplicationFramework Feature or Bugfix:Bugfix Binary Source: No
    Signed-off-by: wujiqin <wujiqin5@huawei.com>
    Change-Id: Idb7e9d1c9507812bc01e24d3afc2ac56f5f9fb4f

---
+++ frameworks/ans/test/unittest/notification_bundle_option_test.cpp
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#define private public
+#define protected public
+#include "notification_bundle_option.h"
+#undef private
+#undef protected
+
+using namespace testing::ext;
+namespace OHOS {
+namespace Notification {
+class NotificationBundleOptionTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp() {}
+    void TearDown() {}
+};
+
+/**
+ * @tc.name: SetBundleName_00001
+ * @tc.desc: Test SetBundleName parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WBBH
+ */
+HWTEST_F(NotificationBundleOptionTest, SetBundleName_00001, Function | SmallTest | Level1)
+{
+    std::string bundleName = "BundleName";
+    std::string bundleName1 = "BundleName1";
+    int32_t uid = 10;
+    auto rrc = std::make_shared<NotificationBundleOption>(bundleName, uid);
+    rrc->SetBundleName(bundleName1);
+    EXPECT_EQ(rrc->GetBundleName(), bundleName1);
+}
+
+/**
+ * @tc.name: SetUid_00001
+ * @tc.desc: Test SetUid parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WBBH
+ */
+HWTEST_F(NotificationBundleOptionTest, SetUid_00001, Function | SmallTest | Level1)
+{
+    std::string bundleName = "BundleName";
+    int32_t uid = 10;
+    int32_t uid1 = 20;
+    auto rrc = std::make_shared<NotificationBundleOption>(bundleName, uid);
+    rrc->SetUid(uid1);
+    EXPECT_EQ(rrc->GetUid(), uid1);
+}
+
+/**
+ * @tc.name: Dump_00001
+ * @tc.desc: Test Dump parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WBBH
+ */
+HWTEST_F(NotificationBundleOptionTest, Dump_00001, Function | SmallTest | Level1)
+{
+    std::string bundleName = "BundleName";
+    int32_t uid = 10;
+    auto rrc = std::make_shared<NotificationBundleOption>(bundleName, uid);
+    std::string ret = "NotificationBundleOption{ bundleName = BundleName, uid = 10 }";
+    EXPECT_EQ(rrc->Dump(), ret);
+}
+
+/**
+ * @tc.name: Marshalling_00001
+ * @tc.desc: Test Marshalling parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WBBH
+ */
+HWTEST_F(NotificationBundleOptionTest, Marshalling_00001, Function | SmallTest | Level1)
+{
+    Parcel parcel;
+    std::string bundleName = "BundleName";
+    int32_t uid = 10;
+    auto rrc = std::make_shared<NotificationBundleOption>(bundleName, uid);
+    EXPECT_EQ(rrc->Marshalling(parcel), true);
+}
+
+/**
+ * @tc.name: Unmarshalling_00001
+ * @tc.desc: Test Unmarshalling parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WBBH
+ */
+HWTEST_F(NotificationBundleOptionTest, Unmarshalling_001, Function | SmallTest | Level1)
+{
+    bool unmarshalling = true;
+    Parcel parcel;
+    std::string bundleName = "BundleName";
+    int32_t uid = 10;
+    std::shared_ptr<NotificationBundleOption> result =
+    std::make_shared<NotificationBundleOption>(bundleName, uid);
+
+    if (nullptr != result) {
+        if (nullptr == result->Unmarshalling(parcel)) {
+            unmarshalling = false;
+        }
+    }
+    EXPECT_EQ(unmarshalling, false);
+}
+
+/**
+ * @tc.name: ReadFromParcel_00001
+ * @tc.desc: Test ReadFromParcel parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WBBH
+ */
+HWTEST_F(NotificationBundleOptionTest, ReadFromParcel_00001, Function | SmallTest | Level1)
+{
+    Parcel parcel;
+    std::string bundleName = "BundleName";
+    int32_t uid = 10;
+    auto rrc = std::make_shared<NotificationBundleOption>(bundleName, uid);
+    EXPECT_EQ(rrc->ReadFromParcel(parcel), false);
+}
+}
+}
---
+++ frameworks/ans/test/unittest/notification_subscribe_info_test.cpp
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#define private public
+#define protected public
+#include "notification_subscribe_info.h"
+#undef private
+#undef protected
+
+using namespace testing::ext;
+namespace OHOS {
+namespace Notification {
+class NotificationSubscribeInfoTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp() {}
+    void TearDown() {}
+};
+
+/**
+ * @tc.name: AddAppName_00001
+ * @tc.desc: Test AddAppName parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WRQ2
+ */
+HWTEST_F(NotificationSubscribeInfoTest, AddAppName_00001, Function | SmallTest | Level1)
+{
+    std::string appName = "AppName";
+    NotificationSubscribeInfo subscribeInfo;
+    auto rrc = std::make_shared<NotificationSubscribeInfo>(subscribeInfo);
+    rrc->AddAppName(appName);
+    std::vector<std::string> result = rrc->GetAppNames();
+    EXPECT_EQ(result.size(), 1);
+}
+
+/**
+ * @tc.name: AddAppNames_00001
+ * @tc.desc: Test AddAppNames parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WRQ2
+ */
+HWTEST_F(NotificationSubscribeInfoTest, AddAppNames_00001, Function | SmallTest | Level1)
+{
+    std::vector<std::string> appNames;
+    NotificationSubscribeInfo subscribeInfo;
+    auto rrc = std::make_shared<NotificationSubscribeInfo>(subscribeInfo);
+    rrc->AddAppNames(appNames);
+    std::vector<std::string> result = rrc->GetAppNames();
+    EXPECT_EQ(result.size(), 0);
+}
+
+/**
+ * @tc.name: AddAppUserId_00001
+ * @tc.desc: Test AddAppUserId parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WRQ2
+ */
+HWTEST_F(NotificationSubscribeInfoTest, AddAppUserId_00001, Function | SmallTest | Level1)
+{
+    int32_t userId = 10;
+    NotificationSubscribeInfo subscribeInfo;
+    auto rrc = std::make_shared<NotificationSubscribeInfo>(subscribeInfo);
+    rrc->AddAppUserId(userId);
+    EXPECT_EQ(rrc->GetAppUserId(), userId);
+}
+
+/**
+ * @tc.name: Marshalling_00001
+ * @tc.desc: Test Marshalling parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WRQ2
+ */
+HWTEST_F(NotificationSubscribeInfoTest, Marshalling_00001, Function | SmallTest | Level1)
+{
+    Parcel parcel;
+    NotificationSubscribeInfo subscribeInfo;
+    auto rrc = std::make_shared<NotificationSubscribeInfo>(subscribeInfo);
+    EXPECT_EQ(rrc->Marshalling(parcel), true);
+}
+
+/**
+ * @tc.name: Unmarshalling_00001
+ * @tc.desc: Test Unmarshalling parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WRQ2
+ */
+HWTEST_F(NotificationSubscribeInfoTest, Unmarshalling_001, Function | SmallTest | Level1)
+{
+    bool unmarshalling = true;
+    Parcel parcel;
+    NotificationSubscribeInfo subscribeInfo;
+    std::shared_ptr<NotificationSubscribeInfo> result =
+    std::make_shared<NotificationSubscribeInfo>(subscribeInfo);
+
+    if (nullptr != result) {
+        if (nullptr == result->Unmarshalling(parcel)) {
+            unmarshalling = false;
+        }
+    }
+    EXPECT_EQ(unmarshalling, true);
+}
+
+/**
+ * @tc.name: ReadFromParcel_00001
+ * @tc.desc: Test ReadFromParcel parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WRQ2
+ */
+HWTEST_F(NotificationSubscribeInfoTest, ReadFromParcel_00001, Function | SmallTest | Level1)
+{
+    Parcel parcel;
+    NotificationSubscribeInfo subscribeInfo;
+    auto rrc = std::make_shared<NotificationSubscribeInfo>(subscribeInfo);
+    EXPECT_EQ(rrc->ReadFromParcel(parcel), true);
+}
+}
+}
