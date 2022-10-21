ce7a3bf4dd6fc8ce194f33fab5493c41a662db73
liuyanzhi <liuyanzhi3@huawei.com>
2022-10-21 20:25:14 +0800

    add fuzz test
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: I1e3874d8a124f0157cbe4b722adb2e1a9030ebd1

---
+++ test/fuzztest/reminderrequestcontinuate_fuzzer/reminderrequestcontinuate_fuzzer.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define private public
+#define protected public
+#include "reminder_request.h"
+#undef private
+#undef protected
+#include "reminderrequestcontinuate_fuzzer.h"
+
+namespace OHOS {
+    bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+    {
+        std::string stringData(data);
+        int32_t reminderId = static_cast<int32_t>(GetU32Data(data));
+        Notification::ReminderRequest reminderRequest(reminderId);
+        reminderRequest.GetButtonInfo();
+        return reminderRequest.GetUid(reminderId, stringData);
+    }
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    char *ch = ParseData(data, size);
+    if (ch != nullptr && size >= GetU32Size()) {
+        OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+        free(ch);
+        ch = nullptr;
+    }
+    return 0;
+}
