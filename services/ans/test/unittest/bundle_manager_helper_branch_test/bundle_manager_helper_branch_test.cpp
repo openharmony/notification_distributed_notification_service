3b298b290481a590cbe16800f9a03313b85c8906
liuyanzhi <liuyanzhi3@huawei.com>
2022-12-30 07:48:14 +0000

    add ans test
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: I135f4abce28fc8483bfed726bb07b1297f2cf6c5

---
+++ services/ans/test/unittest/bundle_manager_helper_branch_test/bundle_manager_helper_branch_test.cpp
@@ -0,0 +1,226 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <functional>
+#include <gtest/gtest.h>
+
+#include "ans_ut_constant.h"
+#define private public
+#define protected public
+#include "bundle_manager_helper.h"
+#undef private
+#undef protected
+
+extern void mockGetSystemAbilityManager(bool mockRet);
+
+using namespace testing::ext;
+namespace OHOS {
+namespace Notification {
+class BundleManagerHelperBranchTest : public testing::Test {
+public:
+    static void SetUpTestCase() {};
+    static void TearDownTestCase() {};
+    void SetUp() {};
+    void TearDown() {};
+};
+
+/**
+ * @tc.number    : BundleManagerHelper_00100
+ * @tc.name      : BundleManagerHelper_00100
+ * @tc.desc      : test GetBundleNameByUid function and bundleMgr_ != nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelperTest_00100, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(false);
+    int32_t uid = 1;
+    EXPECT_EQ("", bundleManagerHelper.GetBundleNameByUid(uid));
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_00200
+ * @tc.name      : BundleManagerHelper_00200
+ * @tc.desc      : test GetBundleNameByUid function and bundleMgr_ == nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_00200, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(true);
+    int32_t uid = 1;
+    EXPECT_EQ("", bundleManagerHelper.GetBundleNameByUid(uid));
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_00300
+ * @tc.name      : BundleManagerHelper_00300
+ * @tc.desc      : test IsSystemApp function and bundleMgr_ != nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_00300, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(false);
+    int32_t uid = 1;
+    EXPECT_EQ(false, bundleManagerHelper.IsSystemApp(uid));
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_00400
+ * @tc.name      : BundleManagerHelper_00400
+ * @tc.desc      : test IsSystemApp function and bundleMgr_ == nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_00400, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(true);
+    int32_t uid = 1;
+    EXPECT_EQ(false, bundleManagerHelper.IsSystemApp(uid));
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_00500
+ * @tc.name      : BundleManagerHelper_00500
+ * @tc.desc      : test GetBundleInfoByBundleName function and bundleMgr_ == nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_00500, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(true);
+    bundleManagerHelper.Connect();
+    std::string bundle = "aa";
+    int32_t userId = 1;
+    AppExecFwk::BundleInfo bundleInfo;
+    EXPECT_EQ(false, bundleManagerHelper.GetBundleInfoByBundleName(bundle, userId, bundleInfo));
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_00600
+ * @tc.name      : BundleManagerHelper_00600
+ * @tc.desc      : test GetBundleInfoByBundleName function and bundleMgr_ != nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_00600, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(false);
+    bundleManagerHelper.Connect();
+    std::string bundle = "aa";
+    int32_t userId = 1;
+    AppExecFwk::BundleInfo bundleInfo;
+    EXPECT_EQ(false, bundleManagerHelper.GetBundleInfoByBundleName(bundle, userId, bundleInfo));
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_00700
+ * @tc.name      : BundleManagerHelper_00700
+ * @tc.desc      : test CheckApiCompatibility function and GetBundleInfoByBundleName is false
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_00700, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    sptr<NotificationBundleOption> bundleOption = new NotificationBundleOption(TEST_DEFUALT_BUNDLE, SYSTEM_APP_UID);
+    EXPECT_EQ(false, bundleManagerHelper.CheckApiCompatibility(bundleOption));
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_00800
+ * @tc.name      : BundleManagerHelper_00800
+ * @tc.desc      : test Connect function and systemAbilityManager is nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_00800, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(true);
+    bundleManagerHelper.Connect();
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_00900
+ * @tc.name      : BundleManagerHelper_00900
+ * @tc.desc      : test Connect function and systemAbilityManager is not nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_00900, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(false);
+    // test Connect and bundleMgr_ == nullptr
+    bundleManagerHelper.Connect();
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_01000
+ * @tc.name      : BundleManagerHelper_01000
+ * @tc.desc      : test Connect function and bundleMgr_ != nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_01000, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(false);
+    bundleManagerHelper.Connect();
+    // test Connect and bundleMgr_ != nullptr
+    bundleManagerHelper.Connect();
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_01100
+ * @tc.name      : BundleManagerHelper_01100
+ * @tc.desc      : test Disconnect function and bundleMgr_ != nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_01100, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(false);
+    bundleManagerHelper.Connect();
+    bundleManagerHelper.Disconnect();
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_01200
+ * @tc.name      : BundleManagerHelper_01200
+ * @tc.desc      : test Disconnect function and bundleMgr_ == nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_01200, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    bundleManagerHelper.Disconnect();
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_01300
+ * @tc.name      : BundleManagerHelper_01300
+ * @tc.desc      : test GetDefaultUidByBundleName function and bundleMgr_ == nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_01300, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(true);
+    std::string bundle = "aa";
+    int32_t userId = 1;
+    EXPECT_EQ(-1, bundleManagerHelper.GetDefaultUidByBundleName(bundle, userId));
+}
+
+/**
+ * @tc.number    : BundleManagerHelper_01400
+ * @tc.name      : BundleManagerHelper_01400
+ * @tc.desc      : test GetDefaultUidByBundleName function and bundleMgr_ != nullptr
+ */
+HWTEST_F(BundleManagerHelperBranchTest, BundleManagerHelper_01400, Function | SmallTest | Level1)
+{
+    BundleManagerHelper bundleManagerHelper;
+    mockGetSystemAbilityManager(false);
+    std::string bundle = "aa";
+    int32_t userId = 1;
+    EXPECT_EQ(-1, bundleManagerHelper.GetDefaultUidByBundleName(bundle, userId));
+}
+}  // namespace Notification
+}  // namespace OHOS
