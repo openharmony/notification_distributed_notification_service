8f4a2d4fd3864be0082a941a54324ded6e89275e
liuyanzhi <liuyanzhi3@huawei.com>
2023-04-10 06:58:20 +0000

    fix tdd
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: I844c8dc4e8a65d1d7bc81d17e493edd20741ba68

--- frameworks/test/moduletest/ans_innerkits_module_publish_test.cpp
+++ frameworks/test/moduletest/ans_innerkits_module_publish_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -101,7 +101,9 @@ public:
 
     void OnCanceled(const std::shared_ptr<Notification> &request,
         const std::shared_ptr<NotificationSortingMap> &sortingMap, int deleteReason) override
-    {}
+    {
+        OnCanceledReceived = true;
+    }
 
     void OnConsumed(const std::shared_ptr<Notification> &request) override
     {
