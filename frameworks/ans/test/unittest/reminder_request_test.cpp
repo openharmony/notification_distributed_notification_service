efae8f08d8cb15adec66b543e09d68d83ccf2559
liuyanzhi <liuyanzhi3@huawei.com>
2023-08-17 11:05:34 +0000

    fix tdd
    Signed-off-by: liuyanzhi <liuyanzhi3@huawei.com>
    Change-Id: I9028c3e70354842c11f7a8830aac7e49e18c0d44

--- frameworks/ans/test/unittest/reminder_request_test.cpp
+++ frameworks/ans/test/unittest/reminder_request_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -406,9 +406,8 @@ HWTEST_F(ReminderRequestTest, IsShowing_00200, Function | SmallTest | Level1)
  */
 HWTEST_F(ReminderRequestTest, OnDateTimeChange_00100, Function | SmallTest | Level1)
 {
-    MockNowInstantMilli(true);
     auto rrc = std::make_shared<ReminderRequestChild>();
-    rrc->isExpired_ = true;
+    rrc->SetExpired(true);
     EXPECT_EQ(rrc->OnDateTimeChange(), false);
 }
 
