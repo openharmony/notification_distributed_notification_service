50f0589b232bb8c06da1645b20ed82731466ae68
baozeyu <baozeyu1@huawei.com>
2023-12-01 10:28:29 +0800

    添加本地实况窗UT用例
    Signed-off-by: baozeyu <baozeyu1@huawei.com>
    Change-Id: I07975658caecc4f4271fe86b4430ef6f10110264

---
+++ frameworks/ans/test/unittest/notification_button_option_test.cpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <string>
+#include <unistd.h>
+#include "notification_button_option.h"
+
+using namespace testing::ext;
+namespace OHOS {
+namespace Notification {
+class NotificationButtonOptionTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp() {}
+    void TearDown() {}
+};
+
+/**
+ * @tc.name: SetButtonName_00001
+ * @tc.desc: Test buttonName_ parameters.
+ * @tc.type: FUNC
+ * @tc.require: issue
+ */
+HWTEST_F(NotificationButtonOptionTest, SetButtonName_00001, Function | SmallTest | Level1)
+{
+    auto rrc = std::make_shared<NotificationButtonOption>();
+    std::string buttonName = "testbuttonName";
+    rrc->SetButtonName(buttonName);
+    EXPECT_EQ(rrc->GetButtonName(), buttonName);
+}
+
+/**
+ * @tc.name: Dump_00001
+ * @tc.desc: Test buttonNames_ dump.
+ * @tc.type: FUNC
+ * @tc.require: issue
+ */
+HWTEST_F(NotificationButtonOptionTest, Dump_00001, Function | SmallTest | Level1)
+{
+    auto rrc = std::make_shared<NotificationButtonOption>();
+    std::string buttonName = "testbuttonName";
+    rrc->SetButtonName(buttonName);
+    EXPECT_EQ(rrc->Dump(), "NotificationButtonOption{ "
+        "buttonName = " + buttonName +" }");
+}
+
+/**
+ * @tc.name: FromJson_00001
+ * @tc.desc: Test FromJson parameters.
+ * @tc.type: FUNC
+ * @tc.require: issue
+ */
+HWTEST_F(NotificationButtonOptionTest, FromJson_00001, Function | SmallTest | Level1)
+{
+    auto rrc = std::make_shared<NotificationButtonOption>();
+    nlohmann::json jsonObject = nlohmann::json{"buttonName"};
+    EXPECT_EQ(jsonObject.is_object(), false);
+    EXPECT_EQ(rrc->FromJson(jsonObject), nullptr);
+}
+
+/**
+ * @tc.name: Marshalling_00001
+ * @tc.desc: Test Marshalling parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WBBH
+ */
+HWTEST_F(NotificationButtonOptionTest, Marshalling_00001, Function | SmallTest | Level1)
+{
+    Parcel parcel;
+    auto rrc = std::make_shared<NotificationButtonOption>();
+    EXPECT_EQ(rrc->Marshalling(parcel), true);
+}
+
+/**
+ * @tc.name: Unmarshalling_00001
+ * @tc.desc: Test Unmarshalling parameters.
+ * @tc.type: FUNC
+ * @tc.require: issueI5WBBH
+ */
+HWTEST_F(NotificationButtonOptionTest, Unmarshalling_00001, Function | SmallTest | Level1)
+{
+    bool unmarshalling = true;
+    Parcel parcel;
+    std::shared_ptr<NotificationButtonOption> result =
+        std::make_shared<NotificationButtonOption>();
+
+    if (nullptr != result) {
+        if (nullptr == result->Unmarshalling(parcel)) {
+            unmarshalling = false;
+        }
+    }
+    EXPECT_EQ(unmarshalling, true);
+}
+}
+}
