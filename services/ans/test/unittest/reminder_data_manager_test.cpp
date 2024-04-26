e34606223e907f41609d9a50ebceef5c6b09c52a
Jasperjiao <jiaoqunzhi@huawei.com>
2024-04-26 18:03:29 +0800

    手机重启补发提醒
    Signed-off-by: Jasperjiao <jiaoqunzhi@huawei.com>

--- services/ans/test/unittest/reminder_data_manager_test.cpp
+++ services/ans/test/unittest/reminder_data_manager_test.cpp
@@ -770,7 +770,7 @@ HWTEST_F(ReminderDataManagerTest, InitStartExtensionAbility_0001, Level1)
     ReminderRequestCalendar* calendar = static_cast<ReminderRequestCalendar*>(reminder.GetRePtr());
     uint64_t not = calendar->GetNowInstantMilli();
     calendar->SetDateTime(now-50000);
-    calendar->SetEndDateTime(not+50000);
+    calendar->SetEndDateTime(now+50000);
     manager->reminderVector_.push_back(reminder);
     manager->Init(true);
     EXPECT_TRUE(!manager->reminderVector_.empty());
