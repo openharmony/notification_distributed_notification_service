bc3a8539316f009e581f72f67fac3dbae550e5c0
侯志雄 <houzhixiong1@h-partners.com>
2024-08-03 11:55:36 +0000

    update frameworks/ans/src/notification_helper.cpp.
    Signed-off-by: 侯志雄 <houzhixiong1@h-partners.com>

--- frameworks/ans/src/notification_helper.cpp
+++ frameworks/ans/src/notification_helper.cpp
@@ -12,7 +12,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #include "notification_helper.h"
 #include "ans_notification.h"
 #include "singleton.h"
