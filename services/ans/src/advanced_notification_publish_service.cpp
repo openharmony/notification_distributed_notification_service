89609b33ad0a3e82a955c942535c8a20ce1db4c4
Mark_Yao <yaozhiyuan@huawei.com>
2024-08-22 11:23:49 +0000

    回退 'Pull Request !2032 : IsSpecialBundleAllowedNotify权限校验逻辑优化'

--- services/ans/src/advanced_notification_publish_service.cpp
+++ services/ans/src/advanced_notification_publish_service.cpp
@@ -59,7 +59,6 @@ constexpr int32_t HOURS_IN_ONE_DAY = 24;
 const static std::string NOTIFICATION_EVENT_PUSH_AGENT = "notification.event.PUSH_AGENT";
 constexpr int32_t RSS_PID = 3051;
 constexpr int32_t ANS_UID = 5523;
-constexpr int32_t BROKER_UID = 5557;
 constexpr int32_t TYPE_CODE_DOWNLOAD = 8;
 constexpr const char *FOCUS_MODE_REPEAT_CALLERS_ENABLE = "1";
 constexpr const char *CONTACT_DATA = "datashare:///com.ohos.contactsdataability/contacts/contact_data?Proxy=true";
@@ -1071,8 +1070,7 @@ ErrCode AdvancedNotificationService::IsSpecialBundleAllowedNotify(
     }
 
     int32_t callingUid = IPCSkeleton::GetCallingUid();
-    if ((callingUid != ANS_UID && callingUid != BROKER_UID)
-        && !AccessTokenHelper::CheckPermission(OHOS_PERMISSION_NOTIFICATION_CONTROLLER)) {
+    if (callingUid != ANS_UID && !AccessTokenHelper::CheckPermission(OHOS_PERMISSION_NOTIFICATION_CONTROLLER)) {
         return ERR_ANS_PERMISSION_DENIED;
     }
 
@@ -1347,8 +1345,7 @@ ErrCode AdvancedNotificationService::RemoveNotification(const sptr<NotificationB
         return ERR_ANS_NON_SYSTEM_APP;
     }
 
-    int32_t callingUid = IPCSkeleton::GetCallingUid();
-    if (callingUid != BROKER_UID && !AccessTokenHelper::CheckPermission(OHOS_PERMISSION_NOTIFICATION_CONTROLLER)) {
+    if (!AccessTokenHelper::CheckPermission(OHOS_PERMISSION_NOTIFICATION_CONTROLLER)) {
         std::string message = "no acl controller permission.";
         OHOS::Notification::HaMetaMessage haMetaMessage = HaMetaMessage(4, 5)
             .ErrorCode(ERR_ANS_PERMISSION_DENIED).NotificationId(notificationId);
