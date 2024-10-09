0606a821bc81458d126253cb47f3de7170aa9cae
xdongs <xdongs@163.com>
2024-10-09 15:42:23 +0800

    删除弹窗缓存
    Signed-off-by: xdongs <xdongs@163.com>

--- interfaces/inner_api/notification_helper.h
+++ interfaces/inner_api/notification_helper.h
@@ -364,6 +364,13 @@ public:
      */
     static ErrCode CanPopEnableNotificationDialog(sptr<AnsDialogHostClient> &hostClient,
         bool &canPop, std::string &bundleName);
+    
+    /**
+     * @brief remove enable notification dialog.
+     *
+     * @return Returns remove dialog result.
+     */
+    static ErrCode RemoveEnableNotificationDialog();
 
     /**
      * @brief Allow the current application to publish notifications on a specified device.
