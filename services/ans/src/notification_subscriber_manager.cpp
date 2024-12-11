ad67c4e20602f26dfa24ef24019972f09c8449c7
luojingsong <luojingsong@h-partners.com>
2024-12-11 16:50:19 +0800

    同步最新的订阅代码
    Signed-off-by: luojingsong <luojingsong@h-partners.com>
    Change-Id: Ibe1c698641445b933a0bcf1d355976be22da7d3a

--- services/ans/src/notification_subscriber_manager.cpp
+++ services/ans/src/notification_subscriber_manager.cpp
@@ -44,8 +44,8 @@ struct NotificationSubscriberManager::SubscriberRecord {
     int32_t userId {SUBSCRIBE_USER_INIT};
     std::string deviceType {CURRENT_DEVICE_TYPE};
     int32_t subscriberUid {DEFAULT_UID};
-    std::vector<NotificationConstant::SlotType> slotTypes {};
     int32_t filterType {0};
+    std::set<NotificationConstant::SlotType> slotTypes {};
 };
 
 NotificationSubscriberManager::NotificationSubscriberManager()
@@ -346,6 +346,10 @@ void NotificationSubscriberManager::AddRecordInfo(
             record->bundleList_.insert(bundle);
             record->subscribedAll = false;
         }
+        record->slotTypes.clear();
+        for (auto slotType : subscribeInfo->GetSlotTypes()) {
+            record->slotTypes.insert(slotType);
+        }
         record->userId = subscribeInfo->GetAppUserId();
         // deviceType is empty, use default
         if (!subscribeInfo->GetDeviceType().empty()) {
@@ -357,6 +361,7 @@ void NotificationSubscriberManager::AddRecordInfo(
     } else {
         record->bundleList_.clear();
         record->subscribedAll = true;
+        record->slotTypes.clear();
     }
 }
 
@@ -542,6 +547,13 @@ bool NotificationSubscriberManager::IsSubscribedBysubscriber(
     bool isSubscribedTheNotification = record->subscribedAll || (iter != record->bundleList_.end()) ||
         (notification->GetNotificationRequestPoint()->GetCreatorUid() == record->subscriberUid);
     if (!isSubscribedTheNotification) {
+        return false;
+    }
+    auto soltType = notification->GetNotificationRequestPoint()->GetSlotType();
+    ANS_LOGI("slotTypecount:%{public}d", (int)record->slotTypes.size());
+    auto slotIter = std::find(record->slotTypes.begin(), record->slotTypes.end(), soltType);
+    bool isSubscribedSlotType = (record->slotTypes.size() == 0) || (slotIter != record->slotTypes.end());
+    if (!isSubscribedSlotType) {
         return false;
     }
 
