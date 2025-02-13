8a2f6b21dc0e35d4d0799762a1851b45b90cd502
luojingsong <luojingsong@h-partners.com>
2025-02-13 19:29:47 +0800

    告警处理
    Signed-off-by: luojingsong <luojingsong@h-partners.com>
    Change-Id: I2a47e2f4f8c40455ee6da8c906cc52ff51fa6f6e

--- frameworks/ans/src/notification_capsule.cpp
+++ frameworks/ans/src/notification_capsule.cpp
@@ -239,11 +239,10 @@ bool NotificationCapsule::ReadFromParcel(Parcel &parcel)
         return false;
     }
 
-    auto vsize = parcel.ReadInt32();
-    int32_t maxSize = static_cast<int32_t>(BUTTON_MAX_SIZE);
-    vsize = (vsize < maxSize) ? vsize : maxSize;
+    auto vsize = static_cast<uint32_t>(parcel.ReadInt32());
+    vsize = (vsize < BUTTON_MAX_SIZE) ? vsize : BUTTON_MAX_SIZE;
     capsuleButton_.clear();
-    for (int i = 0; i < vsize; ++i) {
+    for (uint32_t i = 0; i < vsize; ++i) {
         auto btn = parcel.ReadParcelable<NotificationIconButton>();
         if (btn == nullptr) {
             ANS_LOGE("Failed to read card button");
