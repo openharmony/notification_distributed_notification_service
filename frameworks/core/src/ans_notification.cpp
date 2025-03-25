32b62996c0f06a876939086b51ff0c5b0cc5ccc5
songbao1 <luojingsong@h-partners.com>
2025-03-25 17:04:26 +0800

    回退图片处理
    Signed-off-by: songbao1 <luojingsong@h-partners.com>

--- frameworks/core/src/ans_notification.cpp
+++ frameworks/core/src/ans_notification.cpp
@@ -1437,13 +1437,13 @@ ErrCode AnsNotification::CheckImageSize(const NotificationRequest &request)
     auto littleIcon = request.GetLittleIcon();
     if (NotificationRequest::CheckImageOverSizeForPixelMap(littleIcon, MAX_ICON_SIZE)) {
         ANS_LOGE("The size of little icon exceeds limit");
-        return ERR_ANS_ICON_SIZE;
+        return ERR_ANS_ICON_OVER_SIZE;
     }
 
     auto overlayIcon = request.GetOverlayIcon();
     if (overlayIcon && NotificationRequest::CheckImageOverSizeForPixelMap(overlayIcon, MAX_ICON_SIZE)) {
         ANS_LOGE("The size of overlay icon exceeds limit");
-        return ERR_ANS_ICON_SIZE;
+        return ERR_ANS_ICON_OVER_SIZE;
     }
 
     ErrCode err = request.CheckImageSizeForContent();
