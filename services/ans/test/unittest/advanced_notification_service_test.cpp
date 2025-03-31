cb529ba38295a504aa81568346a221e058620b5b
songbao1 <luojingsong@h-partners.com>
2025-03-31 17:32:16 +0800

    tdd覆盖率
    Signed-off-by: songbao1 <luojingsong@h-partners.com>

--- services/ans/test/unittest/advanced_notification_service_test.cpp
+++ services/ans/test/unittest/advanced_notification_service_test.cpp
@@ -4051,7 +4051,7 @@ HWTEST_F(AdvancedNotificationServiceTest, SetNotificationRemindType_00004, Funct
 HWTEST_F(AdvancedNotificationServiceTest, SetNotificationRemindType_00005, Function | SmallTest | Level1)
 {
     advancedNotificationService_->localScreenOn_ = true;
-    advancedNotificationService_->distributedReminderPolicy_ = 
+    advancedNotificationService_->distributedReminderPolicy_ =
         NotificationConstant::DistributedReminderPolicy::DO_NOT_REMIND;
     auto reminderType = advancedNotificationService_->GetRemindType();
     ASSERT_EQ(reminderType, NotificationConstant::RemindType::DEVICE_ACTIVE_DONOT_REMIND);
