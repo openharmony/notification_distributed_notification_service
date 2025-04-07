2128fb07075d44d208323a8574ac0788e328558b
songbao1 <luojingsong@h-partners.com>
2025-04-07 16:26:25 +0800

    告警处理
    Signed-off-by: songbao1 <luojingsong@h-partners.com>

--- services/distributed/src/soft_bus/distributed_publish_service.cpp
+++ services/distributed/src/soft_bus/distributed_publish_service.cpp
@@ -339,7 +339,8 @@ void DistributedService::RemoveNotifications(const std::shared_ptr<TlvBox>& boxM
         std::string slotTypeString;
         while (slotTypesStream >> slotTypeString) {
             if (!slotTypeString.empty()) {
-                AnalyticsUtil::GetInstance().OperationalReporting(OPERATION_DELETE_BRANCH, atoi(slotTypeString.c_str()));
+                AnalyticsUtil::GetInstance().OperationalReporting(
+                    OPERATION_DELETE_BRANCH, atoi(slotTypeString.c_str()));
             }
         }
     } else {
