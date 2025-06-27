b164c97648c5ca38cad0d973f35ca247441da128
wangdi <wangdi154@huawei.com>
2025-06-27 12:59:03 +0800

    add advancednotificationservice fuzz template
    Signed-off-by: wangdi <wangdi154@huawei.com>

---
+++ test/fuzztest/advancednotificationservice/serviceupdatenotificationtimerbyuid_fuzzer/service_updatenotificationtimerbyuid_fuzzer.cpp
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "service_updatenotificationtimerbyuid_fuzzer.h"
+
+#include <fuzzer/FuzzedDataProvider.h>
+#include "advanced_notification_service.h"
+#include "ans_permission_def.h"
+#include "mock_notification_request.h"
+
+namespace OHOS {
+namespace Notification {
+    bool DoSomethingInterestingWithMyAPI(FuzzedDataProvider *fuzzData)
+    {
+        return true;
+    }
+}
+}
+
+/* Fuzzer entry point */
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    FuzzedDataProvider fdp(data, size);
+    OHOS::Notification::DoSomethingInterestingWithMyAPI(&fdp);
+    return 0;
+}
