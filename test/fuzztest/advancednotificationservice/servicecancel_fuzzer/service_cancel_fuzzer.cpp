a5186f41f61e3c0ea47c34e7e7d0816fdc6e322b
wangsen1994 <wangsen51@huawei.com>
2025-07-01 10:53:26 +0800

    新增Fuzz测试用例
    Signed-off-by: wangsen1994 <wangsen51@huawei.com>

--- test/fuzztest/advancednotificationservice/servicecancel_fuzzer/service_cancel_fuzzer.cpp
+++ test/fuzztest/advancednotificationservice/servicecancel_fuzzer/service_cancel_fuzzer.cpp
@@ -23,6 +23,12 @@ namespace OHOS {
 namespace Notification {
     bool DoSomethingInterestingWithMyAPI(FuzzedDataProvider *fuzzData)
     {
+        auto service = AdvancedNotificationService::GetInstance();
+
+        int32_t notificationId = fuzzData->ConsumeIntegral<int32_t>();
+        std::string label = fuzzData->ConsumeRandomLengthString();
+        std::string instanceKey = fuzzData->ConsumeRandomLengthString();
+        service->Cancel(notificationId, label, instanceKey);
         return true;
     }
 }
@@ -33,6 +39,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
     FuzzedDataProvider fdp(data, size);
+    std::vector<std::string> requestPermission = {
+        OHOS::Notification::OHOS_PERMISSION_NOTIFICATION_CONTROLLER,
+        OHOS::Notification::OHOS_PERMISSION_NOTIFICATION_AGENT_CONTROLLER,
+        OHOS::Notification::OHOS_PERMISSION_SET_UNREMOVABLE_NOTIFICATION
+    };
+    MockRandomToken(&fdp, requestPermission);
     OHOS::Notification::DoSomethingInterestingWithMyAPI(&fdp);
     return 0;
 }
--- test/fuzztest/advancednotificationservice/servicegetdonotdisturbdate_fuzzer/service_getdonotdisturbdate_fuzzer.cpp
+++ test/fuzztest/advancednotificationservice/servicegetdonotdisturbdate_fuzzer/service_getdonotdisturbdate_fuzzer.cpp
@@ -23,6 +23,10 @@ namespace OHOS {
 namespace Notification {
     bool DoSomethingInterestingWithMyAPI(FuzzedDataProvider *fuzzData)
     {
+        auto service = AdvancedNotificationService::GetInstance();
+
+        sptr<NotificationDoNotDisturbDate> date;
+        service->GetDoNotDisturbDate(date);
         return true;
     }
 }
@@ -33,6 +37,12 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     /* Run your code on data */
     FuzzedDataProvider fdp(data, size);
+    std::vector<std::string> requestPermission = {
+        OHOS::Notification::OHOS_PERMISSION_NOTIFICATION_CONTROLLER,
+        OHOS::Notification::OHOS_PERMISSION_NOTIFICATION_AGENT_CONTROLLER,
+        OHOS::Notification::OHOS_PERMISSION_SET_UNREMOVABLE_NOTIFICATION
+    };
+    MockRandomToken(&fdp, requestPermission);
     OHOS::Notification::DoSomethingInterestingWithMyAPI(&fdp);
     return 0;
 }
