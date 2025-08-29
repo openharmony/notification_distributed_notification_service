6e329ea51fd7c1cb1312bbecdbf0549463cafbd9
xdongs <xdongs@163.com>
2025-08-29 10:27:12 +0800

    修复静默通知clone问题
    Signed-off-by: xdongs <xdongs@163.com>

--- services/ans/src/advanced_notification_utils.cpp
+++ services/ans/src/advanced_notification_utils.cpp
@@ -2037,12 +2037,11 @@ void AdvancedNotificationService::UpdateCloneBundleInfoFoSilentReminder(
     const NotificationCloneBundleInfo cloneBundleInfo, const sptr<NotificationBundleOption> bundle)
 {
     auto enableStatus = cloneBundleInfo.GetSilentReminderEnabled();
-    if (enableStatus != NotificationConstant::SWITCH_STATE::SYSTEM_DEFAULT_OFF &&
-        enableStatus != NotificationConstant::SWITCH_STATE::SYSTEM_DEFAULT_ON) {
-        if (NotificationPreferences::GetInstance()->SetSilentReminderEnabled(bundle,
-            enableStatus == NotificationConstant::SWITCH_STATE::USER_MODIFIED_ON ? true : false) != ERR_OK) {
-            ANS_LOGW("SetSilentReminderEnabled failed.");
-        }
+    if (NotificationPreferences::GetInstance()->SetSilentReminderEnabled(bundle,
+    (enableStatus == NotificationConstant::SWITCH_STATE::USER_MODIFIED_ON ||
+    enableStatus == NotificationConstant::SWITCH_STATE::SYSTEM_DEFAULT_ON) ?
+    true : false) != ERR_OK) {
+        ANS_LOGW("SetSilentReminderEnabled failed.");
     }
 }
 
