0335252ac5ad963a5af842133331077fc0190273
he-guokai <heguokai6@huawei-partners.com>
2025-10-10 18:27:09 +0800

    修改lockscreenPicture解析失败的问题
    Signed-off-by: he-guokai <heguokai6@huawei-partners.com>

--- frameworks/ets/ani/src/sts_notification_content.cpp
+++ frameworks/ets/ani/src/sts_notification_content.cpp
@@ -1173,7 +1173,7 @@ bool SetNotificationBasicContent(
         ANS_LOGD("SetNotificationBasicContent: set additionalText failed");
     }
     ani_ref lockScreenPicObj = CreateAniPixelMap(env, basicContent->GetLockScreenPicture());
-    if (lockScreenPicObj == nullptr || !SetPropertyByRef(env, object, "lockScreenPicture", lockScreenPicObj)) {
+    if (lockScreenPicObj == nullptr || !SetPropertyByRef(env, object, "lockscreenPicture", lockScreenPicObj)) {
         ANS_LOGD("SetNotificationBasicContent: set lockScreenPicture failed");
     }
     ANS_LOGD("SetNotificationBasicContent end");
@@ -1488,10 +1488,6 @@ bool WarpNotificationLiveViewContent(
     if (content == nullptr) {
         ANS_LOGE("SetNotificationLiveViewContent: get LiveViewContent failed");
         return false;
-    }
-    ani_object lockScreenPicObj = CreateAniPixelMap(env, content->GetLockScreenPicture());
-    if (lockScreenPicObj == nullptr || !SetPropertyByRef(env, contentObj, "lockScreenPicture", lockScreenPicObj)) {
-        ANS_LOGD("SetNotificationLiveViewContent: set lockScreenPicture failed");
     }
     ani_enum_item enumItem = nullptr;
     if (!LiveViewStatusCToEts(env, content->GetLiveViewStatus(), enumItem)
