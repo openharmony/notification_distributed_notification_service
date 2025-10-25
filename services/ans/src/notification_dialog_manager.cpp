3dd5d600fc4e7fa7feea6ca50847271383ba69fa
xdongs <xdongs@163.com>
2025-10-25 11:02:18 +0800

    通知授权弹窗点击允许不允许增加HA事件
    Signed-off-by: xdongs <xdongs@163.com>

--- services/ans/src/notification_dialog_manager.cpp
+++ services/ans/src/notification_dialog_manager.cpp
@@ -254,6 +254,10 @@ bool NotificationDialogManager::OnDialogButtonClicked(const std::string& bundleN
         // Do not return here, need to clear the data
     }
     ans_.SendDialogClickHiSysEvent(bundleOption, enabled);
+    HaMetaMessage message = HaMetaMessage(EventSceneId::SCENE_28, EventBranchId::BRANCH_0);
+    message.Message(bundleOption->GetBundleName() + "_" + std::to_string(bundleOption->GetUid()) +
+        " allow:" + std::to_string(enabled));
+    NotificationAnalyticsUtil::ReportModifyEvent(message);
     EnabledDialogStatus status = enabled ? EnabledDialogStatus::ALLOW_CLICKED : EnabledDialogStatus::DENY_CLICKED;
     return HandleOneDialogClosed(bundleOption, status);
 }
