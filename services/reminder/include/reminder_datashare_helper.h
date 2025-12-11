ea34907bd423046d5b3baeb7d4f65b5e00adf41c
qq_42718467 <2656338986@qq.com>
2025-12-11 20:51:11 +0800

    update：更新日历霸屏提醒逻辑
    Signed-off-by: qq_42718467 <2656338986@qq.com>

--- services/reminder/include/reminder_datashare_helper.h
+++ services/reminder/include/reminder_datashare_helper.h
@@ -227,6 +227,13 @@ private:
         sptr<ReminderRequest>& reminder);
     void BuildReminderV1(const DataShare::DataShareObserver::ChangeInfo::VBucket& info,
         sptr<ReminderRequest>& reminder);
+
+    /**
+     * @brief Build query predicates conditions.
+     *
+     * Create predicates for querying valid alerts.
+     */
+    DataShare::DataSharePredicates BuildQueryPredicates(int64_t timestamp, int64_t targetTimestamp);
 private:
     // Singleton
     ReminderDataShareHelper();
