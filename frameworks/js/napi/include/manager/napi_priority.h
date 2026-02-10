8dae1484874e78648d296ffb96ff94108a4d1182
wangdi <wangdi154@huawei.com>
2026-02-10 11:41:14 +0800

    fix 6.1 diff v1
    Signed-off-by: wangdi <wangdi154@huawei.com>

--- frameworks/js/napi/include/manager/napi_priority.h
+++ frameworks/js/napi/include/manager/napi_priority.h
@@ -27,8 +27,6 @@ napi_value NapiIsPriorityEnabled(napi_env env, napi_callback_info info);
 napi_value NapiIsPriorityEnabledByBundle(napi_env env, napi_callback_info info);
 napi_value NapiSetBundlePriorityConfig(napi_env env, napi_callback_info info);
 napi_value NapiGetBundlePriorityConfig(napi_env env, napi_callback_info info);
-napi_value NapiSetPriorityEnabledByBundles(napi_env env, napi_callback_info info);
-napi_value NapiGetPriorityEnabledByBundles(napi_env env, napi_callback_info info);
 }  // namespace NotificationNapi
 }  // namespace OHOS
 
