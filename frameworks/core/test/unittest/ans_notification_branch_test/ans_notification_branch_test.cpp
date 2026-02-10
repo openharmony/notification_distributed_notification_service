c64f7fbc77c0d6b53ac68465af4d4368731b4822
wangdi <wangdi154@huawei.com>
2026-02-10 11:41:14 +0800

    fix 6.1 diff v1
    Signed-off-by: wangdi <wangdi154@huawei.com>

--- frameworks/core/test/unittest/ans_notification_branch_test/ans_notification_branch_test.cpp
+++ frameworks/core/test/unittest/ans_notification_branch_test/ans_notification_branch_test.cpp
@@ -756,17 +756,6 @@ public:
         return ERR_ANS_INVALID_PARAM;
     }
 
-    ErrCode GetPriorityEnabledByBundles(const std::vector<sptr<NotificationBundleOption>> &bundleOptions,
-        std::map<sptr<NotificationBundleOption>, bool> &priorityEnable) override
-    {
-        return ERR_ANS_INVALID_PARAM;
-    }
-
-    ErrCode SetPriorityEnabledByBundles(const std::map<sptr<NotificationBundleOption>, bool> &priorityEnable) override
-    {
-        return ERR_ANS_INVALID_PARAM;
-    }
-
     ErrCode SetBundlePriorityConfig(
         const sptr<NotificationBundleOption> &bundleOption, const std::string &value) override
     {
