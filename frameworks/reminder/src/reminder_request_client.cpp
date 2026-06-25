4b210e9787ab81d7d1e650f7272d893765e62962
let_it_rot <gaojiaqi7@h-partners.com>
2026-06-25 20:57:12 +0800

    Adjust the interface
    Signed-off-by: let_it_rot <gaojiaqi7@h-partners.com>

--- frameworks/reminder/src/reminder_request_client.cpp
+++ frameworks/reminder/src/reminder_request_client.cpp
@@ -19,14 +19,12 @@
 #include "ans_inner_errors.h"
 #include "notification_helper.h"
 #include "reminder_agent_service_proxy.h"
-#include "reminder_service_load_callback.h"
 
 #include "ipc_skeleton.h"
 #include "iservice_registry.h"
 #include "system_ability_definition.h"
 
 namespace OHOS::Notification {
-static constexpr int32_t REMINDER_SERVICE_LOADSA_TIMEOUT_MS = 10000;
 static constexpr int32_t REMINDER_AGENT_SERVICE_ID = 3204;
 
 ErrCode ReminderRequestClient::AddSlotByType(const NotificationConstant::SlotType& slotType)
@@ -219,46 +217,19 @@ sptr<IReminderAgentService> ReminderRequestClient::GetReminderServiceProxy()
 bool ReminderRequestClient::LoadReminderService()
 {
     std::unique_lock<ffrt::mutex> lock(serviceLock_);
-    sptr<ReminderServiceCallback> loadCallback = sptr<ReminderServiceCallback>(new ReminderServiceCallback());
-    if (loadCallback == nullptr) {
-        ANSR_LOGE("loadCallback is nullptr.");
-        return false;
-    }
-
     auto samgrProxy = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
     if (samgrProxy == nullptr) {
         ANSR_LOGE("GetSystemAbilityManager failed.");
         return false;
     }
 
-    int32_t ret = samgrProxy->LoadSystemAbility(REMINDER_AGENT_SERVICE_ID, loadCallback);
-    if (ret != ERR_OK) {
+    sptr<IRemoteObject> remoteObject = samgrProxy->GetSystemAbility(REMINDER_AGENT_SERVICE_ID);
+    if (remoteObject == nullptr) {
         ANSR_LOGE("Failed to Load systemAbility.");
         return false;
     }
-
-    auto waitStatus = proxyConVar_.wait_for(lock, std::chrono::milliseconds(REMINDER_SERVICE_LOADSA_TIMEOUT_MS),
-        [this]() { return proxy_ != nullptr; });
-    if (!waitStatus) {
-        ANSR_LOGE("Load reminder service timeout.");
-        return false;
-    }
+    proxy_ = iface_cast<IReminderAgentService>(remoteObject);
     return true;
-}
-
-void ReminderRequestClient::LoadSystemAbilitySuccess(const sptr<IRemoteObject>& remoteObject)
-{
-    std::lock_guard<ffrt::mutex> lock(serviceLock_);
-    if (remoteObject != nullptr) {
-        proxy_ = iface_cast<IReminderAgentService>(remoteObject);
-        proxyConVar_.notify_one();
-    }
-}
-
-void ReminderRequestClient::LoadSystemAbilityFail()
-{
-    std::lock_guard<ffrt::mutex> lock(serviceLock_);
-    proxy_ = nullptr;
 }
 
 void ReminderRequestClient::StartReminderAgentService()
